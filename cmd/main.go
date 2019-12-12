119ebe2b735131127866900c4133f3711c9cc14b
opt1k <evil@cryp.email>
2019-12-12 16:44:51 +0500

    clean arch with versioning

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,4 @@
 package main
 
-import (
-	"context"
-	"database/sql"
-	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
-)
-
 func main() {
-	ctx := context.Background()
-	dataStoreName := fmt.Sprintf("%s:%s@tcp(%s)/%s",
-		"localhost:3306",
-		"alex",
-		"rocket2288",
-		"axxon",
-	)
-
-	db, err := sql.Open("mysql", dataStoreName)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-
-	c, err := db.Conn(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer c.Close()
 }
